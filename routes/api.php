6eaedce384e08b26b21acae66afbd917e64ff073
Nabil-nl <nabilouhmida9@gmail.com>
2024-07-04 12:46:37 +0100

    add memeber

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,27 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-// sanctum 
+// login register logout 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
-
-
+//  books 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/books', [BookController::class, 'index']);
     Route::post('/books', [BookController::class, 'store']);
     Route::get('/books/{book}', [BookController::class, 'show']);
     Route::put('/books/{book}', [BookController::class, 'update']);
     Route::delete('/books/{book}', [BookController::class, 'destroy']);
+});
+//member
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('members', MemberController::class);
 });
